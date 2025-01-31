edda92b1a67c8bf0f3fa32ff6148ad186dd88b3c
prove-ability <prove.ability00@gmail.com>
2025-02-01 01:39:17 +0900

    chore: Update HTML language attribute to Korean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="ko" className={`${GeistSans.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
